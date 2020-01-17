77a2ad24715b6051b3db1059314e6af0f0439092
Ikechukwu Kalu <ikechukwuakalu@gmail.com>
2020-01-17 09:57:01 +0100

    Initial commit

---
+++ mock_data/coins.js
@@ -0,0 +1,100 @@
+const coinsPrices = [
+  {
+    name: "Bitcoin",
+    symbol: "BTC",
+    price: 10109,
+    change: 0.9,
+    image: "../../images/coins/bitcoin.svg"
+  },
+  {
+    name: "Ethereum",
+    symbol: "ETH",
+    price: 1109.003,
+    change: 1.9,
+    image: "../../images/coins/ethereum.svg"
+  },
+  {
+    name: "Ripple",
+    symbol: "XRP",
+    price: 909.89,
+    change: 0.67,
+    image: "../../images/coins/ripple.svg"
+  },
+  {
+    name: "Bitcoin Cash",
+    symbol: "BCH",
+    price: 809.12,
+    change: 0.9,
+    image: "../../images/coins/bitcoin-cash.svg"
+  },
+  {
+    name: "Litecoin",
+    symbol: "LTC",
+    price: 609,
+    change: 6.9,
+    image: "../../images/coins/litecoin.svg"
+  },
+  {
+    name: "Ethereum Classic",
+    symbol: "ETC",
+    price: 809,
+    change: 4.79,
+    image: "../../images/coins/ethereum-classic.svg"
+  },
+  {
+    name: "Binance coin",
+    symbol: "BNB",
+    price: 209.93,
+    change: 3.9,
+    image: "../../images/coins/binance-coin.svg"
+  },
+  {
+    name: "Bitcoin",
+    symbol: "BTC",
+    price: 10109,
+    change: 0.9,
+    image: "../../images/coins/bitcoin.svg"
+  },
+  {
+    name: "Ethereum",
+    symbol: "ETH",
+    price: 1109.003,
+    change: 1.9,
+    image: "../../images/coins/ethereum.svg"
+  },
+  {
+    name: "Ripple",
+    symbol: "XRP",
+    price: 909.89,
+    change: 0.67,
+    image: "../../images/coins/ripple.svg"
+  },
+  {
+    name: "Bitcoin Cash",
+    symbol: "BCH",
+    price: 809.12,
+    change: 0.9,
+    image: "../../images/coins/bitcoin-cash.svg"
+  },
+  {
+    name: "Litecoin",
+    symbol: "LTC",
+    price: 609,
+    change: 6.9,
+    image: "../../images/coins/litecoin.svg"
+  },
+  {
+    name: "Ethereum Classic",
+    symbol: "ETC",
+    price: 809,
+    change: 4.79,
+    image: "../../images/coins/ethereum-classic.svg"
+  },
+  {
+    name: "Binance coin",
+    symbol: "BNB",
+    price: 209.93,
+    change: 3.9,
+    image: "../../images/coins/binance-coin.svg"
+  }
+];
